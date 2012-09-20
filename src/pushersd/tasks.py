fa3adccfb93d7378473a868926e49b7f9538b7bf
Chris Hannam <bassdread@gmail.com>
2012-09-20 21:13:33 +0100

    html

--- src/pushersd/tasks.py
+++ src/pushersd/tasks.py
@@ -4,7 +4,25 @@ from celery.task import task
 
 import pusher
 from serverdensity.api import SDApi, SDServiceError
+from local_settings import *
 
 @celery.task
 def hello():
-	print "hello world"
+    pusher.app_id = PUSHER_APP_ID
+    pusher.key = PUSHER_KEY
+    pusher.secret = PUSHER_SECRET
+
+    api = SDApi(
+        URL,
+        USERNAME,
+        PASSWORD,
+        API_KEY
+    )
+
+    try:
+        alerts = api.alerts.getLast()
+        alerts = alerts['data']['alerts']
+        p = pusher.Pusher()
+        p['test_channel'].trigger(‘my_event’, {‘alerts’: alerts})
+    except SDServiceError, error:
+        pass
